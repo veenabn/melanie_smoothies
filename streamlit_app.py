aa489adfe3b60c68a64f3a1167ea56419f087f34
veenabn <168031591+veenabn@users.noreply.github.com>
2024-04-24 15:24:31 -0700

    Update streamlit_app.py
    Added Serach_on and test the output

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,9 @@ name_on_order = st.text_input('Name on the Smoothie:')
 st.write('Name on your Smoothie will be ',name_on_order  )
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredient_list = st.multiselect('choose up to 5 ingredients:', my_dataframe,max_selections=5)
 
