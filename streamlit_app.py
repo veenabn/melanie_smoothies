66eb3de8a54550d18694fc469668cbdd86fdb027
veenabn <168031591+veenabn@users.noreply.github.com>
2024-04-24 14:33:08 -0700

    Update streamlit_app.py
    Replaced  Watermelon with variable -fruit_chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ if ingredient_list:
     ingredients_string = ''
     for fruit_chosen in ingredient_list:
         ingredients_string +=fruit_chosen + ' '
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen )
         fv_df = st.dataframe( data = fruityvice_response.json(),use_container_width=True)
 
 
