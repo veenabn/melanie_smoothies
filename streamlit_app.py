1a6d1f02b4c04b1222822edb52e820b7659cb509
veenabn <168031591+veenabn@users.noreply.github.com>
2024-04-24 15:38:27 -0700

    Update streamlit_app.py
    Added search on iloc(0)

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,10 +18,10 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
+# Convert Snowflake dataframe to pandas snowframe
 pd_df= my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
-
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredient_list = st.multiselect('choose up to 5 ingredients:', my_dataframe,max_selections=5)
 
@@ -31,6 +31,9 @@ if ingredient_list:
     ingredients_string = ''
     for fruit_chosen in ingredient_list:
         ingredients_string +=fruit_chosen + ' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        st.subheader(fruit_chosen + 'Nutrition Informatuion')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen )
         fv_df = st.dataframe( data = fruityvice_response.json(),use_container_width=True)
 
