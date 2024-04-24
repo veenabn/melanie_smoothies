5203b7dd998656fb2b219b10dcde7ecaee8c29eb
veenabn <168031591+veenabn@users.noreply.github.com>
2024-04-24 14:22:03 -0700

    Update streamlit_app.py
    Add json response instead of response.

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,4 +36,5 @@ if ingredient_list:
         st.success('Your Smoothie is ordered ' +  name_on_order + '!', icon="✅")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)     
+#st.text(fruityvice_response)   
+st.text(fruityvice_response.json())
