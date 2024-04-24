f36fba01291fb8160e6ad846bf4cf72e63b2131c
veenabn <168031591+veenabn@users.noreply.github.com>
2024-04-24 14:18:36 -0700

    Update streamlit_app.py
    Added  API request call to fruityvice.com

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,3 +34,6 @@ if ingredient_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered ' +  name_on_order + '!', icon="✅")
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)     
